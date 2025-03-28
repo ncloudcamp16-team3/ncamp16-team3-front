3d23447807840148c69320f065580946a7008439
taesun <taesun4767@gmail.com>
2025-03-28 12:29:36 +0900

    [FIX]: import 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./css/App.css";
 import Layout1Outlet from "./components/Global/Layout1.jsx";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main.jsx";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/User/Login.jsx";
 import Petsta from "./pages/Petsta/Petsta.jsx";
 import Board from "./pages/Board/Board.jsx";
 import Reserve from "./pages/Reserve/Reserve.jsx";
