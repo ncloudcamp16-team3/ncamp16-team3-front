799dc416dd4d1e8cce313876449f0b7844e42d83
pgw30508 <pgw30508@daum.net>
2025-03-31 09:47:36 +0900

    임시저장

--- src/components/User/Step1.jsx
+++ src/components/User/Step1.jsx
@@ -1,12 +1,65 @@
 import React from "react";
+import joinLogo from "/src/assets/images/User/join_logo.png";
+import KakaoLogo from "/src/assets/images/User/Kakao Login.png";
+import NaverLogo from "/src/assets/images/User/Naver Login.png";
+import GoogleLogo from "/src/assets/images/User/Google Login.png";
 
 const Step1 = ({ nextStep }) => {
+    // return (
+    //     <div className="flex flex-col">
+    //         첫번째페이지
+    //         <button className="border border-black" onClick={nextStep}>
+    //             다음
+    //         </button>
+    //     </div>
+    // );
+
     return (
         <div className="flex flex-col">
-            첫번째페이지
-            <button className="border border-black" onClick={nextStep}>
-                다음
-            </button>
+            <div className="flex flex-col items-center justify-center min-h-screen p-5">
+                <h1 className="text-3xl font-bold text-gray-800 mb-4">
+                    꼬리친구들
+                </h1>
+                <img src={joinLogo} className="w-32 h-32 mb-4" alt="logo" />
+                <p className="text-lg text-gray-600 mb-6">
+                    다양한 애완동물을 위한 컨텐츠
+                </p>
+
+                <div className="flex flex-col gap-4">
+                    <button
+                        onClick={nextStep}
+                        className="p-3 bg-yellow-400 hover:bg-yellow-500 rounded-lg"
+                    >
+                        <img
+                            src={KakaoLogo}
+                            className="w-40 h-auto"
+                            alt="Kakao Login"
+                        />
+                    </button>
+
+                    <button
+                        onClick={nextStep}
+                        className="p-3 bg-green-500 hover:bg-green-600 rounded-lg"
+                    >
+                        <img
+                            src={NaverLogo}
+                            className="w-40 h-auto"
+                            alt="Naver Login"
+                        />
+                    </button>
+
+                    <button
+                        onClick={nextStep}
+                        className="p-3 bg-white hover:bg-gray-200 rounded-lg shadow-md"
+                    >
+                        <img
+                            src={GoogleLogo}
+                            className="w-40 h-auto"
+                            alt="Google Login"
+                        />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/User/Step2.jsx
+++ src/components/User/Step2.jsx
@@ -2,11 +2,45 @@ import React from "react";
 
 const Step2 = ({ nextStep }) => {
     return (
-        <div className="flex flex-col">
-            두번째페이지
-            <button className="border border-black" onClick={nextStep}>
-                다음
-            </button>
+        // <div className="flex flex-col">
+        //     두번째페이지
+        //     <button className="border border-black" onClick={nextStep}>
+        //         다음
+        //     </button>
+        // </div>
+        <div>
+            <div className="flex max-w-sm w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto">
+                <button className="border border-black" onClick={nextStep}>
+                    다음
+                </button>
+
+                <div className="w-2 bg-gray-800"></div>
+
+                <div className="flex items-center px-2 py-3">
+                    <form className="w-full max-w-sm">
+                        <div className="flex items-center border-b border-gray-400 py-2">
+                            <input
+                                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                                type="text"
+                                placeholder="Jane Doe"
+                                aria-label="Full name"
+                            />
+                            <button
+                                className="flex-shrink-0 bg-green-500 hover:bg-green-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                                type="button"
+                            >
+                                Sign Up
+                            </button>
+                            <button
+                                className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+                                type="button"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
