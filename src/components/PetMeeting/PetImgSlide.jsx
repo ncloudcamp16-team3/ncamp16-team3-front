c82a66efe8fed27da459e1b1373847a215feb3db
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-04-14 12:27:33 +0900

    [FEAT] 애완동물 상세 페이지 ui구현

---
+++ src/components/PetMeeting/PetImgSlide.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { Box } from "@mui/material";
+import RightArrow from "../../assets/images/PetMeeting/right-arrow.svg";
+import LeftArrow from "../../assets/images/PetMeeting/left-arrow.svg";
+
+const PetImgSlide = ({ petDetails }) => {
+    const thumbnailPhoto = petDetails.photos.find((photo) => photo.id === petDetails.thumbnail);
+
+    // 섬네일 가장 앞에 배치하도록 정렬
+    const sortedPhotos = [thumbnailPhoto, ...petDetails.photos.filter((photo) => photo.id !== petDetails.thumbnail)];
+
+    return (
+        <Box
+            sx={{
+                backgroundColor: "#FDF1E5",
+                width: "100%",
+                height: "250px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                overflow: "hidden",
+                position: "relative",
+                borderRadius: "20px",
+                mb: "10px",
+            }}
+        >
+            <Swiper
+                modules={[Pagination, Navigation]}
+                spaceBetween={20}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                navigation={{ nextEl: ".swiper-button-next-custom", prevEl: ".swiper-button-prev-custom" }}
+                loop={true}
+                style={{ height: "100%" }}
+            >
+                {sortedPhotos.map((src, idx) => (
+                    <SwiperSlide key={idx}>
+                        <img
+                            src={src.url}
+                            alt={`slide-${idx}`}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "contain",
+                                display: "block",
+                            }}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <Box
+                className="swiper-button-prev-custom"
+                component="img"
+                src={RightArrow}
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "10px",
+                    zIndex: 10,
+                    transform: "translateY(-50%)",
+                    borderRadius: "50%",
+                    cursor: "pointer",
+                    userSelect: "none",
+                    width: "50px",
+                    height: "50px",
+                }}
+            ></Box>
+            <Box
+                className="swiper-button-next-custom"
+                component="img"
+                src={LeftArrow}
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    right: "10px",
+                    zIndex: 10,
+                    transform: "translateY(-50%)",
+                    borderRadius: "50%",
+                    cursor: "pointer",
+                    userSelect: "none",
+                    width: "50px",
+                    height: "50px",
+                }}
+            ></Box>
+        </Box>
+    );
+};
+
+export default PetImgSlide;
--- src/components/PetMeeting/Petdetails.jsx
+++ src/components/PetMeeting/Petdetails.jsx
@@ -1,95 +1,159 @@
 import React, { useRef } from "react";
-import PetDetails from "../../mock/PetMeeting/petDetails.json";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/modules";
+import PetData from "../../mock/PetMeeting/petData.json";
+import Male from "../../assets/images/PetMeeting/male.svg";
+import Female from "../../assets/images/PetMeeting/female.svg";
+import Theme from "../../theme/theme.js";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import LeftArrow from "../../assets/images/PetMeeting/left-arrow.svg";
-import RightArrow from "../../assets/images/PetMeeting/right-arrow.svg";
 
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import PetImgSlide from "./PetImgSlide.jsx";
+import petTypes from "../../constants/petTypes.js";
 
 const Petdetails = () => {
-    const petDetails = useRef(PetDetails);
-    const friendInfo = petDetails.current;
-    const thumbnailPhoto = friendInfo.photos.find((photo) => photo.id === friendInfo.thumbnail);
+    const petData = useRef(PetData);
+    const petDetails = petData.current;
 
-    // 섬네일 가장 앞에 배치하도록 정렬
-    const sortedPhotos = [thumbnailPhoto, ...friendInfo.photos.filter((photo) => photo.id !== friendInfo.thumbnail)];
+    const getAge = (birthDateString) => {
+        const birthDate = new Date(birthDateString);
+        const today = new Date();
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+        const dayDiff = today.getDate() - birthDate.getDate();
+
+        if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+            age--;
+        }
+
+        return age;
+    };
 
     return (
         <Box
             sx={{
-                backgroundColor: "#ddd",
-                width: "100%",
-                height: "300px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                overflow: "hidden",
+                width: "100% - 20px",
+                margin: "10px",
+                pb: "0",
             }}
         >
-            <Swiper
-                modules={[Pagination, Navigation]}
-                spaceBetween={20}
-                slidesPerView={1}
-                pagination={{ clickable: true }}
-                navigation={{ nextEl: ".swiper-button-next-custom", prevEl: ".swiper-button-prev-custom" }}
-                loop={true}
-                style={{ height: "100%" }}
-            >
-                {sortedPhotos.map((src, idx) => (
-                    <SwiperSlide key={idx}>
-                        <img
-                            src={src.url}
-                            alt={`slide-${idx}`}
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                                objectFit: "contain",
-                                display: "block",
-                            }}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <Box
-                className="swiper-button-prev-custom"
+            <PetImgSlide petDetails={petDetails} />
+            <Typography
                 sx={{
-                    position: "absolute",
-                    top: "50%",
+                    mt: "0",
+                    fontSize: "25px",
+                    display: "inline",
+                    verticalAlign: "middle",
+                }}
+            >
+                {petDetails.name}
+            </Typography>
+            <Box
+                component="img"
+                src={petDetails.gender == "MALE" ? Male : Female}
+                sx={{
+                    verticalAlign: "middle",
+                    color: "blue",
+                }}
+            />
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    m: "10px 0",
+                }}
+            >
+                <Typography
+                    sx={{
+                        p: "5px 15px",
+                        borderRadius: "20px",
+                        backgroundColor: "#FFEBCD",
+                        fontSize: "20px",
+                        display: "inline",
+                        verticalAlign: "middle",
+                        color: "rgba(0, 0, 0, 0.5)",
+                        margin: "0 2%",
+                    }}
+                >
+                    {petTypes[petDetails.petType]}
+                </Typography>
+                <Typography
+                    sx={{
+                        p: "5px 15px",
+                        borderRadius: "20px",
+                        backgroundColor: "#E0CDFF",
+                        fontSize: "20px",
+                        display: "inline",
+                        verticalAlign: "middle",
+                        color: "rgba(0, 0, 0, 0.5)",
+                        margin: "0 2%",
+                    }}
+                >
+                    {getAge(petDetails.birth)}세
+                </Typography>
+                <Typography
+                    sx={{
+                        p: "5px 15px",
+                        borderRadius: "20px",
+                        backgroundColor: "#FFCDD6",
+                        fontSize: "20px",
+                        display: "inline",
+                        verticalAlign: "middle",
+                        color: "rgba(0, 0, 0, 0.5)",
+                        margin: "0 2%",
+                    }}
+                >
+                    {petDetails.weight}KG
+                </Typography>
+            </Box>
+            <Typography
+                sx={{
+                    fontSize: "20px",
+                }}
+            >
+                정보
+            </Typography>
+            <Typography
+                sx={{
+                    fontSize: "16px",
+                    color: "rgba(0, 0, 0, 0.5)",
+                    mb: "70px",
+                }}
+            >
+                생년월일 : {petDetails.birth}
+                <br />
+                중성화 : {petDetails.neutured ? "O" : "X"}
+                <br />
+                소개 : {petDetails.info}
+            </Typography>
+            <Box
+                sx={{
+                    position: "fixed",
+                    bottom: "60px",
+                    width: "100%",
+                    height: "70px",
+                    backgroundColor: "white",
+                    borderRadius: "10px",
+                    color: "white",
+                    zIndex: 999,
+                }}
+            ></Box>
+            <Button
+                onClick={{}}
+                sx={{
+                    position: "fixed",
+                    bottom: "85px",
                     left: "10px",
-                    zIndex: 10,
-                    transform: "translateY(-50%)",
-                    color: "#fff",
-                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    padding: "6px 10px",
-                    borderRadius: "50%",
-                    cursor: "pointer",
-                    userSelect: "none",
-                }}
-            >
-                {LeftArrow}
-            </Box>
-            <Box
-                className="swiper-button-next-custom"
-                sx={{
-                    position: "absolute",
-                    top: "50%",
                     right: "10px",
-                    zIndex: 10,
-                    transform: "translateY(-50%)",
-                    color: "#fff",
-                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    padding: "6px 10px",
-                    borderRadius: "50%",
-                    cursor: "pointer",
-                    userSelect: "none",
+                    backgroundColor: Theme.brand3,
+                    borderRadius: "10px",
+                    color: "white",
+                    zIndex: 1000,
                 }}
             >
-                {RightArrow}
-            </Box>
+                채팅하기
+            </Button>
         </Box>
     );
 };
