0c4b54e2d5e04c45ec85a360b22f54dbca856ae2
pgw30508 <pgw30508@daum.net>
2025-04-16 14:57:41 +0900

    [MODIFY] 소셜로그인 버튼 변경

--- src/components/User/SocialLogin.jsx
+++ src/components/User/SocialLogin.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import JoinLogo from "../../assets/images/User/join-logo.png";
 import KakaoLogo from "../../assets/images/User/kakao-logo.svg";
-import NaverLogo from "../../assets/images/User/naver-logo.svg";
+//import NaverLogo from "../../assets/images/User/naver-logo.svg";
+import NaverLogo from "../../assets/images/User/naver-logo2.png";
 import GoogleLogo from "../../assets/images/User/google-logo.svg";
 
 const SocialLogin = () => {
@@ -36,59 +37,85 @@ const SocialLogin = () => {
             </Typography>
 
             <Box width="90%" display="flex" flexDirection="column" alignItems="center" gap={2}>
-                <Box
-                    display="flex"
-                    border="1px solid #747775"
-                    borderRadius="10px"
-                    height="53px"
-                    padding="12px"
-                    alignItems="center"
-                    gap="10px"
-                    backgroundColor="#FEE500"
+                <Button
+                    variant="contained"
                     onClick={() => handleOAuthLogin("kakao")}
-                    width="90%"
+                    sx={{
+                        backgroundColor: "#FEE500",
+                        color: "black",
+                        textTransform: "none",
+                        height: "53px",
+                        borderRadius: "10px",
+                        padding: "12px",
+                        width: "90%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-start",
+                        gap: "10px",
+                        "&:hover": {
+                            backgroundColor: "#ffdf00",
+                        },
+                    }}
                 >
-                    <Box width="32px">
+                    <Box width="32px" display="flex" justifyContent="center" alignItems="center">
                         <Box component="img" src={KakaoLogo} alt="Kakao Login" width="18px" />
                     </Box>
-                    <Typography fontSize="14px">KAKAO로 계속하기</Typography>
-                </Box>
-                <Box
-                    display="flex"
-                    border="1px solid #747775"
-                    borderRadius="10px"
-                    height="53px"
-                    padding="12px"
-                    alignItems="center"
-                    gap="10px"
-                    backgroundColor="#27C250"
-                    width="90%"
+                    <Typography fontSize="14px">Kakao로 계속하기</Typography>
+                </Button>
+
+                <Button
+                    variant="contained"
                     onClick={() => handleOAuthLogin("naver")}
+                    sx={{
+                        backgroundColor: "#00C300",
+                        color: "white",
+                        textTransform: "none",
+                        height: "53px",
+                        borderRadius: "10px",
+                        padding: "12px",
+                        width: "90%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-start",
+                        gap: "10px",
+                        "&:hover": {
+                            backgroundColor: "#00b000",
+                        },
+                    }}
                 >
-                    <Box width="32px">
-                        <Box component="img" src={NaverLogo} alt="Naver Login" width="32px" />
+                    <Box width="32px" display="flex" justifyContent="center" alignItems="center">
+                        <Box component="img" src={NaverLogo} alt="Naver Login" width="16px" />
                     </Box>
-                    <Typography color="white" fontSize="14px">
-                        NAVER로 계속하기
-                    </Typography>
-                </Box>
-                <Box
-                    display="flex"
-                    border="1px solid #747775"
-                    borderRadius="10px"
-                    height="53px"
-                    padding="12px"
-                    alignItems="center"
-                    gap="10px"
-                    backgroundColor="#FFFFFF"
-                    width="90%"
+                    <Typography fontSize="14px">Naver로 계속하기</Typography>
+                </Button>
+
+                <Button
+                    variant="contained"
                     onClick={() => handleOAuthLogin("google")}
+                    sx={{
+                        backgroundColor: "#FFFFFF",
+                        color: "black",
+                        textTransform: "none",
+                        height: "53px",
+                        borderRadius: "10px",
+                        padding: "12px",
+                        width: "90%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-start",
+                        gap: "10px",
+                        borderColor: "#C4C4C4",
+                        "&:hover": {
+                            backgroundColor: "#f5f5f5",
+                            borderColor: "#aaa",
+                        },
+                    }}
                 >
-                    <Box width="32px">
+                    <Box width="32px" display="flex" justifyContent="center" alignItems="center">
                         <Box component="img" src={GoogleLogo} alt="Google Login" width="18px" />
                     </Box>
                     <Typography fontSize="14px">Google로 계속하기</Typography>
-                </Box>
+                </Button>
             </Box>
         </Box>
     );
