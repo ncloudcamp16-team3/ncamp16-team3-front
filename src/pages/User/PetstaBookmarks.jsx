fc5ee20db7ab7aef8b8b64d9c80b544899f9e934
juyoung <eunh9372@naver.com>
2025-04-17 12:06:04 +0900

    [FEAT]북마크 조회/상세보기

--- src/pages/User/PetstaBookmarks.jsx
+++ src/pages/User/PetstaBookmarks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box,Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import TitleBar from "../../components/Global/TitleBar.jsx";
 
