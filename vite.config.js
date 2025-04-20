9feace201d2dfd0a641772f20590d18c2e3dca3b
pgw30508 <pgw30508@daum.net>
2025-04-20 19:00:05 +0900

    [FIX] vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -50,7 +50,7 @@ export default defineConfig(({ mode }) => {
                     : mode === "production"
                       ? {
                             "/api": {
-                                target: "http://tailfriends.kro.kr:8080",
+                                target: "https://tailfriends.kro.kr:8080",
                                 changeOrigin: true,
                                 secure: false,
                             },
