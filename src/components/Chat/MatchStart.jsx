fefcbab1e4e988a1577cc917e21668077cddedd0
taesun <taesun4767@gmail.com>
2025-05-01 18:06:15 +0900

    [FIX]검색 늘어나는부분 수정

--- src/components/Chat/MatchStart.jsx
+++ src/components/Chat/MatchStart.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { Box, Typography, Avatar } from "@mui/material";
 
 const MatchStart = ({ participants, text }) => {
+    console.log(participants);
     return (
         <Box textAlign="center" py={2}>
             <Box display="flex" justifyContent="center" gap={2} mb={1}>
                 {participants.map((p, index) => (
                     <Box key={index} textAlign="center">
-                        <Avatar
-                            src={`/mock/Global/images/${p.photo}`}
-                            alt={p.name}
-                            sx={{ width: 60, height: 60, margin: "0 auto" }}
-                        />
+                        <Avatar src={p.photo} alt={p.name} sx={{ width: 60, height: 60, margin: "0 auto" }} />
                         <Typography variant="body2">
                             {p.name} {p.age}
                         </Typography>
