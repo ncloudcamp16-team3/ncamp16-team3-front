f301757ea0b50cc5a6eae66586df1e1f6d8933c7
김태현 <kokiyo1030@naver.com>
2025-05-01 17:24:43 +0900

    [FIX] 관리자 업체등록 이미지 수정 2차

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,6 @@ import AdminLayout from "./components/Admin/AdminLayout.jsx";
 // Protected Routes
 import ProtectedRoute from "./components/User/ProtectedRoute.jsx";
 import ProtectedAdminRoute from "./components/Admin/ProtectedAdminRoute.jsx";
-
-import PetstaMain from "./pages/PetSta/PetstaMain.jsx";
-import PostDetailWrapper from "./pages/PetSta/PostDetailWrapper.jsx";
-
 import EditVideoDetail from "./pages/PetSta/EditVideoDetail.jsx";
 import EditPhotoDetail from "./pages/PetSta/EditPhotoDetail.jsx";
 
@@ -38,7 +34,7 @@ const AddPet = lazy(() => import("./pages/User/AddPet.jsx"));
 const EditPet = lazy(() => import("./pages/User/EditPet.jsx"));
 const Bookmark = lazy(() => import("./pages/User/Bookmark"));
 const PetstaBookmarks = lazy(() => import("./pages/User/PetstaBookmarks"));
-const PostBookmarks = lazy(() => import("./pages/User/PostBookmarks"));
+const PostBookmarks = lazy(() => import("./pages/User/BoardBookmarks.jsx"));
 const PetSitterRegister = lazy(() => import("./pages/Sitter/PetSitterRegister.jsx"));
 const PetSitterFinder = lazy(() => import("./pages/Sitter/PetSitterFinder.jsx"));
 const PetSitterDetail = lazy(() => import("./pages/Sitter/PetSitterDetail.jsx"));
